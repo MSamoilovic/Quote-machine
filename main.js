396fe75f0a7f92c2b9d1bb37b7ea38ceffbbc6bc
Marko Samoilovic <msamoilovic@gmail.com>
2018-02-24 22:14:42 +0100

    Konacno sredio boje

--- main.js
+++ main.js
@@ -26,24 +26,23 @@ function getQuote() {
                 opacity: 1
             }, 500);
             $('#author').text(author)
-        })
-
-        var color = Math.floor(Math.random() * colors.length);
-
-        console.log(colors[color]);
-
-        $('body').animate({
-            backgroundColor : colors[color],
-            color : colors[color] 
-        }, 1000);
-
-        $('.button').animate({
-            backgroundColor : colors[color]
-        }, 1000);
-
+        });
         $('#quote_tw').attr('href', "https://twitter.com/intent/tweet?text=" + randomQuote  +  author);
         $('#tmblr_quote').attr('href', 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+ encodeURIComponent(author) +'&content='+ encodeURIComponent(randomQuote) + '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button')
     })
+
+    var color = Math.floor(Math.random() * colors.length);
+
+    console.log(colors[color]);
+
+    $('body').animate({
+        backgroundColor : colors[color],
+        color : colors[color] 
+    }, "slow");
+
+    $('.button').animate({
+        backgroundColor : colors[color]
+    }, "slow");
 
 }
 
