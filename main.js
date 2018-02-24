bc0aa2c2787792038484f12bfd0697b2a1d87d96
Marko Samoilovic <msamoilovic@gmail.com>
2018-02-24 18:56:08 +0100

    Gotovo 99% app

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function getQuote() {
 
         console.log(colors[color]);
 
-        $('html body').animate({
+        $('body').animate({
             backgroundColor : colors[color],
             color : colors[color] 
         }, 1000);
@@ -41,7 +41,8 @@ function getQuote() {
             backgroundColor : colors[color]
         }, 1000);
 
-        $('#quote_tw').attr('href', "https://twitter.com/intent/tweet?text=" + randomQuote  + author);
+        $('#quote_tw').attr('href', "https://twitter.com/intent/tweet?text=" + randomQuote  +  author);
+        $('#tmblr_quote').attr('href', 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+ encodeURIComponent(author) +'&content='+ encodeURIComponent(randomQuote) + '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button')
     })
 
 }
