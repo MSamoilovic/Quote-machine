1d634865013ff341c2e63cace6d7bd49334a9c51
Marko Samoilovic <msamoilovic@gmail.com>
2018-02-24 14:33:16 +0100

    Popravio problem sa zarezima

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     function getQuote() {
         var url = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
         $.getJSON(url, function (data) {
-            $("#text").html('"' + data.quoteText + '"');
+            $("#text").html(data.quoteText);
             $("#author").html("-" + data.quoteAuthor);
         });
     };
