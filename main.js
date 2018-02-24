47860f91e98a6db79ba5036aeb7d1b5210f3327c
Marko Samoilovic <msamoilovic@gmail.com>
2018-02-24 14:31:07 +0100

    Provalio osnovno u JS

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
-    
-})
+$(document).ready(function () {
+    getQuote();
+
+    var randomQuote;
+    var author;
+    function getQuote() {
+        var url = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+        $.getJSON(url, function (data) {
+            $("#text").html('"' + data.quoteText + '"');
+            $("#author").html("-" + data.quoteAuthor);
+        });
+    };
+});
